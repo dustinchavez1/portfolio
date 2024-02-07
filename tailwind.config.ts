02d17d6f2586240996e61110f464d44efd52bb83
dustinchavez1 <dustin_chavez@csu.fullerton.edu>
2024-02-07 03:19:20 -0800

    type fixing

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -25,11 +25,11 @@ const config: Config = {
       keyframes: {
         fadeInLeft: {
           '0%': {
-            opacity: 0,
+            opacity: "0",
             transform: 'translateX(-100%)',
           },
           '100%': {
-            opacity: 1,
+            opacity: "1",
             transform: 'translateX(0)',
           },
         },
